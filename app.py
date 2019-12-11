f7640fd92c8a101b88c860eb5622c8b433998d27
Romel Fernando <romelfernando09@gmail.com>
2019-12-11 18:18:08 +0800

    testApp with message flashing

--- app.py
+++ app.py
@@ -19,16 +19,34 @@ def index():
         field4 = request.form['field4']
 
         # Old way i do messaging 
-        if field1 and field2 and field3 and field4:
+        '''if field1 and field2 and field3 and field4:
             message = 'Fields validated!'
             css_class = 'alert alert-success'
         else:
             message = 'All fields required!'
-            css_class = 'alert alert-danger
+            css_class = 'alert alert-danger'''
 
-        return render_template('index.html', 
-            message=message,
-            css_class=css_class)
+        if not field1: # field1 == ''
+            flash(u'Field 1 is required!', 'alert alert-danger')
+        else:
+            flash(u'Field 1 is valid!', 'alert alert-success')
+        
+        if not field2: # field2 == ''
+            flash(u'Field 2 is required!', 'alert alert-danger')
+        else:
+            flash(u'Field 2 is valid!', 'alert alert-success')
+        
+        if not field3: # field3 == ''
+            flash(u'Field 3 is required!', 'alert alert-danger')
+        else:
+            flash(u'Field 3 is valid!', 'alert alert-success')
+
+        if not field4: # field4 == ''
+            flash(u'Field 4 is required!', 'alert alert-danger')
+        else:
+            flash(u'Field 4 is valid!', 'alert alert-success')
+
+        return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
